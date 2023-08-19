efc97e541c793911a76c1d0c8341534368b89a5a
valmiralove <n.valmira@live.com>
2023-08-19 11:33:29 -0500

    first commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,12 @@
+package projects;
+
+import java.sql.Connection;
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+
+	}
+}
